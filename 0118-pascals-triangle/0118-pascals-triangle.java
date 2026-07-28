28b53b8993f365fff1fc28004d55ad18f5bf6fff
Vishal Nakum <136720318+VishalNakum1210@users.noreply.github.com>
2026-07-28 10:12:36 +0530

    Time: 1 ms (94.31%), Space: 43.5 MB (52.58%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+class Solution {
+    public List<List<Integer>> generate(int n) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            List<Integer> temp = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    temp.add(1);
+                } else {
+                    int sum = (res.get(i - 1).get(j)) + (res.get(i - 1).get(j - 1));
+                    temp.add(sum);
+                }
+            }
+            res.add(temp);
+        }
+
+        return res;
+    }
+}
