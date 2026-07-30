3413a9d03568da21fa67998e2344184331374529
Vishal Nakum <136720318+VishalNakum1210@users.noreply.github.com>
2026-07-30 10:21:18 +0530

    Time: 1 ms (28.44%), Space: 45.2 MB (75.31%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
@@ -1,10 +1,10 @@
 class Solution {
     public int arraySign(int[] nums) {
-        boolean minAns = false;
+        int pro = 1;
         for(int i = 0; i < nums.length; i++){
             if(nums[i] == 0) return 0;
-            if(nums[i] < 0) minAns = !minAns;
+            if(nums[i] < 0) pro = -pro;
         }
-        return (minAns) ? -1 : 1;
+        return pro;
     }
 }
