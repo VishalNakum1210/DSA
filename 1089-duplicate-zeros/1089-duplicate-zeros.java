7e974c1feaefe374ea327a3bb65b1e6b9b73c5a4
Vishal Nakum <136720318+VishalNakum1210@users.noreply.github.com>
2026-08-11 10:20:08 +0530

    Time: 0 ms (100%), Space: 47 MB (22.55%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.java
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -1,15 +1,18 @@
 class Solution {
     public void duplicateZeros(int[] arr) {
-        int n = arr.length;
+        int[] temp = arr.clone();
+        int index = 0;
+
         for(int i = 0; i < arr.length; i++){
-            if(arr[i] == 0){
-                int j = n-2;
-                while(j >= i){
-                    arr[j+1] = arr[j];
-                    j--;
-                }
-                i++;
+            if(temp[index] == 0){
+                arr[i] = 0;
+                if(i < arr.length-1)
+                    arr[++i] = 0;
             }
+            else{
+                arr[i] = temp[index];
+            }
+            index++;
         }
     }
 }
