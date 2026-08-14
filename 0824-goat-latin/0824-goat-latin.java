23e0a919523a1cf8e9eb4bd87a44b5df3a909c71
Vishal Nakum <136720318+VishalNakum1210@users.noreply.github.com>
2026-08-14 21:02:47 +0530

    Time: 1 ms (100%), Space: 43 MB (84.53%) - LeetHub

--- 0824-goat-latin/0824-goat-latin.java
+++ 0824-goat-latin/0824-goat-latin.java
@@ -1,21 +1,32 @@
 class Solution {
     public String toGoatLatin(String sentence) {
         StringBuilder sb = new StringBuilder();
-        String[] word = sentence.split(" ");
-        for(int i = 0; i <word.length; i++){
-            if(word[i].charAt(0) == 'a' || word[i].charAt(0) == 'e' || word[i].charAt(0) == 'i' || word[i].charAt(0) == 'o' || word[i].charAt(0) == 'u' || word[i].charAt(0) == 'A' || word[i].charAt(0) == 'E' || word[i].charAt(0) == 'I' || word[i].charAt(0) == 'O' || word[i].charAt(0) == 'U'){
-                sb.append(word[i]);
-            }else{
-                sb.append(word[i].substring(1) + word[i].charAt(0));
+        String[] words = sentence.split(" ");
+
+        for (int i = 0; i < words.length; i++) {
+
+            if (isVol(words[i].charAt(0))) {
+                sb.append(words[i]);
+            } else {
+                sb.append(words[i].substring(1));
+                sb.append(words[i].charAt(0));
             }
+
             sb.append("ma");
-            for(int j = 0; j <= i; j++){
+
+            for (int j = 0; j <= i; j++) {
                 sb.append("a");
             }
-            if(i < word.length - 1){
+
+            if (i < words.length - 1) {
                 sb.append(" ");
             }
         }
+
         return sb.toString();
+    }
+
+    public boolean isVol(char ch) {
+        return "aeiouAEIOU".indexOf(ch) != -1;
     }
 }
