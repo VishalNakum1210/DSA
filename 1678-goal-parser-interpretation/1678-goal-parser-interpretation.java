50d79fc494406b4cd860ac5f15f9038c0fb25aec
Vishal Nakum <136720318+VishalNakum1210@users.noreply.github.com>
2026-08-17 08:55:50 +0530

    Time: 0 ms (100%), Space: 42.9 MB (68.93%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -1,17 +1,16 @@
 class Solution {
     public String interpret(String command) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < command.length(); i++) {
-            if (i < command.length() - 1 && command.charAt(i) == '(' && command.charAt(i + 1) == ')') {
-                sb.append("o");
+        for(int i = 0; i < command.length(); i = i + 0){
+            if(command.charAt(i) == 'G'){
+                sb.append("G");
                 i++;
-            }
-            else{
-                if(command.charAt(i) == '(' || command.charAt(i) == ')'){
-                    continue;
-                }else{
-                    sb.append(command.charAt(i));
-                }
+            }else if(command.charAt(i+1) == ')'){
+                sb.append("o");
+                i+=2;
+            }else{
+                sb.append("al");
+                i += 4;
             }
         }
         return sb.toString();
