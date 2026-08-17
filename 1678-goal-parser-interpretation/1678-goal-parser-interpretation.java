99083af8738ea6e4a4195dffd9745b04c962c13b
Vishal Nakum <136720318+VishalNakum1210@users.noreply.github.com>
2026-08-17 08:48:08 +0530

    Time: 1 ms (75.26%), Space: 42.5 MB (96.32%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String interpret(String command) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < command.length(); i++) {
+            if (i < command.length() - 1 && command.charAt(i) == '(' && command.charAt(i + 1) == ')') {
+                sb.append("o");
+            }
+            else{
+                if(command.charAt(i) == '(' || command.charAt(i) == ')'){
+                    continue;
+                }else{
+                    sb.append(command.charAt(i));
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
