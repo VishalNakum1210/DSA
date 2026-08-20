81ae6a7d0a08e2650d13025fe883183e84bb53c4
Vishal Nakum <136720318+VishalNakum1210@users.noreply.github.com>
2026-08-20 08:07:59 +0530

    Time: 2 ms (87.33%), Space: 44.4 MB (88.2%) - LeetHub

---
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String maximumOddBinaryNumber(String s) {
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '1'){
+                count++;
+            }
+        }
+        for(int i = 0; i < s.length() -1; i++){
+            if(count > 1){
+                sb.append('1');
+                count--;
+            }else{
+                sb.append('0');
+            }
+        }
+        sb.append('1');
+        return sb.toString();
+    }
+}
